3141f7eb10646dee479546a8ff2de486fe48ef69
GitHub Action <action@github.com>
2021-09-27 14:18:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/thencuber/intellijlinterplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.thencuber.intellijlinterplugin.services
+
+import com.github.thencuber.intellijlinterplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/thencuber/intellijlinterplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/thencuber/intellijlinterplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.thencuber.intellijlinterplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thencuber.intellijlinterplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
