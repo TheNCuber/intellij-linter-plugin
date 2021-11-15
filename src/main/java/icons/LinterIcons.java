d32fbefbca7347922b99443eb623b6bcd69ca6c6
Noah Bühlmann <33098853+TheNCuber@users.noreply.github.com>
2021-11-16 00:54:03 +0100

    style: add new icons

---
+++ src/main/java/icons/LinterIcons.java
@@ -0,0 +1,8 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+import javax.swing.*;
+
+public class LinterIcons {
+    public static final Icon Refresh_Icon = IconLoader.getIcon("/icons/refresh_16.svg", LinterIcons.class);
+}
